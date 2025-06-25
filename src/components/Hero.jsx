390e9ecc30d9202b6ef6f2f18c854bf2907a3a84
punit <punitdethe2006@gmail.com>
2025-06-26 01:02:21 +0530

    link update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="container mx-auto px-4 py-20 text-center">
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Prana School</h1>
         <p className="text-lg md:text-xl mb-8">Nurturing young minds for a brighter future.</p>
-        <a href="https://google.com" className="bg-white text-blue-600 font-bold py-3 px-6 rounded-full hover:bg-blue-100 transition duration-300">
+        <a href="https://th-thumbnailer.cdn-si-edu.com/f5C0f9GYYNOTGmGH6jX3RMfWaJU=/fit-in/1600x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/64/f2/64f27cfb-3afa-49da-8bcc-2ca99b21f3eb/butt_42-60156647.jpg" className="bg-white text-blue-600 font-bold py-3 px-6 rounded-full hover:bg-blue-100 transition duration-300">
           Enter Asshole
         </a>
       </div>
