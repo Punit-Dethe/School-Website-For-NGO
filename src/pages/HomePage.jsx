4e6785bad22bb766a83aad960598ff99262d84df
punit <punitdethe2006@gmail.com>
2025-07-15 21:00:28 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import Hero from "../New_components/Hero";
 import AboutSection from "../New_components/AboutSection";
@@ -8,6 +9,20 @@ import Activities from "../components/Activities";
 import Curriculum from "../components/Curriculum";
 import Testimonial from "../components/testamonial";
 import FAQ from "../components/FAQ";
+=======
+import React from 'react';
+import Hero from '../New_components/Hero';
+import AboutSection from '../New_components/AboutSection';
+import QuotePage from '../New_components/QuotePage';
+import HopeCard from '../New_components/Hopecard';
+import OurStory from '../components/OurStory';
+import WhatWeDo from '../New_components/WhatWeDo';
+import WhyTheyNeedUs from '../components/WhyTheyNeedUs';
+import Activities from '../components/Activities';
+import Curriculum from '../components/Curriculum';
+import Testimonial from '../components/testamonial';
+import FAQ from '../components/FAQ';
+>>>>>>> 31e813fd2a75a022ddd950fce43eee9eba9ea3a2
 
 const HomePage = () => {
   return (
@@ -16,6 +31,7 @@ const HomePage = () => {
       <QuotePage />
       <AboutSection />
       <HopeCard />
+        <WhatWeDo />
       <Activities />
       <Amenities />
       <Curriculum />
